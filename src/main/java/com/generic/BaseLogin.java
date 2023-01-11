c43ab056db4fd284b7ba3024db11d5a234030c60
studentqa2021 <sarowerhome@gmail.com>
2023-01-11 16:26:20 -0500

    first commit

---
+++ src/main/java/com/generic/BaseLogin.java
@@ -0,0 +1,29 @@
+package com.generic;
+
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseLogin {
+	public void getlogin() {
+		//open a browser
+	WebDriverManager.chromedriver().setup();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
